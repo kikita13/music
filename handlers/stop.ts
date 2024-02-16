56d636d2735854643ab47acdc774a931e43fc245
Nikita Ryabov <qazxswedcxzaqw6@gmail.com>
2024-02-16 18:58:01 +0500

    очищение очереди при stop

--- handlers/stop.ts
+++ handlers/stop.ts
@@ -1,11 +1,14 @@
 import { VoiceConnection } from "@discordjs/voice";
 import { Message } from "discord.js";
+import { QUEUE } from "../consts/queue";
 
 //Выполняется при команде *prefix*stop
 export const stopCommand = (
   connect: VoiceConnection,
   message: Message
 ) => {
+  QUEUE.splice(0, QUEUE.length)
+  
   if (connect.state.status === 'ready') {
     connect.destroy();
   } else {
